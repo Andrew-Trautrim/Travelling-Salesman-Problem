9882a2f32e4ee2194a436c144179f12e0a47b3b5
root <atrautrim7@gmail.com>
2019-01-08 08:47:29 -0500

    commenting

--- christofides.cpp
+++ christofides.cpp
@@ -16,29 +16,28 @@ int main(void) {
 
 	TSP christofide;
 	
-	// step 1 - complete undirected weighted graph, G
+	// complete undirected weighted graph, G
 	// adjacency matrix representing the graph
-	int graph[N][N] = {{ I, 26, 25, 23, 31, 18},
-			   {26,  I, 21, 22, 27, 29},
-			   {25, 21,  I, 24, 19, 20},
-			   {23, 22, 24,  I, 12, 30},
-			   {31, 27, 19, 12,  I, 25},
-			   {18, 29, 20, 30, 25,  I},
+	int graph[N][N] = {{ I, 37, 51, 50, 46},
+			   {37,  I, 44, 41, 48},
+			   {51, 44,  I, 28, 33},
+			   {50, 41, 28,  I, 53},
+			   {46, 48, 33, 53,  I}
 			  }; 
 	cout << "Complete Undirected Wighted Graph - G" << endl;
 	christofide.printMatrix(graph);
 
-	// step 2: calculate minimum spanning tree, T, from graph G
+	// step 1: calculate minimum spanning tree T of G
 	int mst[N][N];
 	christofide.minimumSpanningTree(graph, mst);
 	cout << "Minimum Spanning Tree - T" << endl;
 	christofide.printMatrix(mst);
 
-	// step 3: calculate degree of verticies from T
+	// step 2: calculate odd degree of verticies from T
 	int degree[N];
 	christofide.getVertexDegree(mst, degree);
 
-	// step 4: subgraph of G from odd degree verticies given by T
+	// graph of odd degree verticies O
 	int oddDegree[N][N];
 	christofide.oddDegree(graph, degree, oddDegree);
 	cout << "Odd Degree Verticies of T Subgraph of G - O" << endl;
@@ -48,33 +47,32 @@ int main(void) {
 	christofide.removeCommonEdges(oddDegree, mst);
 	christofide.printMatrix(oddDegree);
 	
-	// step 5
+	// step 3 minimum-weight perfect match
 	// **TODO** MINIMUM-WEIGHT PERFECT MATCH
 	int vertexCount = christofide.vertexCount(oddDegree);
-	int match[N][N] = {{I, I, I, I, I, I},
-			{I, I, I, I, I, I},
-			{I, I, I, I, I, I},
-			{I, I, I, I, I, I},
-			{I, I, I, I, I, I},
-			{I, I, I, I, I, I}
-		       };
-	int visited[N] = {0, 0, 0, 0, 0, 0};
+	int match[N][N] = {{I, I, I, I, I},
+			   {I, I, I, I, I},
+			   {I, I, I, I, I},
+			   {I, I, I, I, I},
+			   {I, I, I, I, I}
+			  };
+	int visited[N] = {0, 0, 0, 0, 0};
 	christofide.perfectMatch(oddDegree, 0, vertexCount, visited, match);
 
 	cout << "Perfect Match of O - M" << endl;
 	christofide.printMatrix(match);
 
-	// step 6: combine minimum-weight perfect match with the minimum spanning tree
+	// step 4: combine minimum-weight perfect match with the minimum spanning tree
 	christofide.combine(mst, match);
 	cout << "M U T" << endl;
 	christofide.printMatrix(mst);
 	
-	// step 7: create Eulerian Circuit from the combined graph
+	// step 5: create Eulerian Circuit from the combined graph
 	int edgeCount = christofide.edgeCount(mst, false);
 	int from[edgeCount], to[edgeCount];
 	christofide.eulerianCircuit(mst, 0, 0, edgeCount, from, to);
 
-	// step 8: remove repeated verticies
+	// step 6: remove repeated verticies creating a hamiltonian circuit
 	int circuit[N][N];
 	christofide.removeRepeatedVerticies(graph, from, to, edgeCount, circuit);
 	cout << "Hamiltonian Circuit" << endl;
